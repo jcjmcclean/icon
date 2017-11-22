42c98cd41735ebc4a6515c095bab8f7b085e4ad6
james <james@melonwebdesign.co.uk>
2017-11-22 09:52:30 +0000

    Added icon directive

---
+++ icon.directive.js
@@ -0,0 +1,20 @@
+angular
+	.module('icon')
+	.directive('icon', icon);
+
+function icon() {
+	return {
+		restrict: 'A',
+		scope:{
+			name: '@'
+		},
+		controller: iconController,
+		template: '<span ng-bind-html="markup"></span>'
+	};
+}
+
+function iconController(ICONS, $sce, $scope) {
+	if(ICONS && ICONS[$scope.name] && ICONS[$scope.name].data) {
+		$scope.markup = $sce.trustAsHtml(ICONS[$scope.name].data);
+	}
+}
